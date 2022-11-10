b134b4b4f3bef42a82625b6c7151ae31a53acebe
Sergio Guerrero <guerrerovillasergio@gmail.com>
2022-11-10 11:15:51 -0500

    fin proyecto rango sass

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('hola mundo');
+
+const inputRango = document.getElementById('inputRango');
+const visitas = document.getElementById('visitas');
+const precio = document.getElementById('precio');
+const arrayVisitas = ['10K', '50K', '100K', '500K', '1M' ];
+
+
+inputRango.addEventListener('input', () => {
+    console.log('mediste click 😜');
+    console.log(inputRango.value);
+
+    precio.textContent = inputRango.value;
+    visitas.textContent = arrayVisitas[(inputRango.value / 8 - 1)];
+})
